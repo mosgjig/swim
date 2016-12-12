828832f9d86e149d520a552995178f6028886291
Vlera <vleracurri@gmail.com>
2016-12-12 20:42:37 +0100

    deleted .ide and target files

--- target/classes/db.migration/V1_9_contacts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE contacts
-(
-    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title VARCHAR(45),
-    first_name VARCHAR(45),
-    middle_name VARCHAR(45),
-    last_name VARCHAR(45)
-);
