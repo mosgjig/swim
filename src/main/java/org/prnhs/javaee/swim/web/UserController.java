20640a9de15d8b2686209ba2674f0a1f238a0d43
Modest Syla <modest.syla@gmail.com>
2016-12-15 20:08:10 +0100

    ms-web - add web layer

---
+++ src/main/java/org/prnhs/javaee/swim/web/UserController.java
@@ -0,0 +1,40 @@
+package org.prnhs.javaee.swim.web;
+
+import java.util.List;
+import org.prnhs.javaee.swim.dto.UserDto;
+import org.prnhs.javaee.swim.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto save(@RequestBody UserDto dto) {
+        return userService.save(dto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto getById(@PathVariable String id) {
+        return userService.getById(id);
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDto> getAll() {
+        return userService.getAll();
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(String id) {
+        userService.delete(id);
+    }
+}
