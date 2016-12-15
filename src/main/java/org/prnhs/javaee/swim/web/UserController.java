f633ba66758599baecf017257e67c2027c1950d7
Modest Syla <modest.syla@gmail.com>
2016-12-15 20:45:49 +0100

    develop - minor cleanup

--- src/main/java/org/prnhs/javaee/swim/web/UserController.java
+++ src/main/java/org/prnhs/javaee/swim/web/UserController.java
@@ -33,8 +33,8 @@ public class UserController {
         return userService.getAll();
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public void delete(String id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable String id) {
         userService.delete(id);
     }
 }
