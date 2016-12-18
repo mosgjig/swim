5361886a2a969da4ec703e87198c305de31c11ff
Vlera <vleracurri@gmail.com>
2016-12-18 17:01:33 +0100

    add ContactController

---
+++ src/main/java/org/prnhs/javaee/swim/web/ContactController.java
@@ -0,0 +1,37 @@
+package org.prnhs.javaee.swim.web;
+
+import org.prnhs.javaee.swim.dto.ContactsDto;
+import org.prnhs.javaee.swim.services.ContactsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactsServices service;
+
+    @RequestMapping(value = "/",method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ContactsDto save(@RequestBody ContactsDto dto) {
+        return service.save(dto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ContactsDto getById(@PathVariable Integer id){
+        return service.getById(id);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<ContactsDto> getAll(){
+        return service.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Integer id){
+        service.delete(id);
+    }
+}
