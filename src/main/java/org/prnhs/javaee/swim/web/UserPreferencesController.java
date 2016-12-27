7b2ccbbf8c18d8412111ba1e2c903847a353e765
ArgitaCanhasi <argitacanhasi@gmail.com>
2016-12-27 21:14:02 +0100

    Added swagger

--- src/main/java/org/prnhs/javaee/swim/web/UserPreferencesController.java
+++ src/main/java/org/prnhs/javaee/swim/web/UserPreferencesController.java
@@ -3,6 +3,10 @@ package org.prnhs.javaee.swim.web;
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.prnhs.javaee.swim.dto.UserPreferencesDto;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "UserPreferences Controller")
 @RequestMapping("/userPreferences")
 public class UserPreferencesController {
 
@@ -38,6 +43,10 @@ public class UserPreferencesController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Save a userPreference", notes = "UserPreferences are either created or updated depending whether a username is present or not.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = UserPreferencesDto.class),
+        @ApiResponse(code = 400, message = "Bad Request")})
     public UserPreferencesDto save(@RequestBody UserPreferencesDto dto) {
         LOGGER.debug("Save method has been called, saving username {}", dto.getUsername());
         Meter requests = metrics.meter("save");
@@ -46,14 +55,21 @@ public class UserPreferencesController {
     }
 
     @RequestMapping(value = "/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Retrive a userPreference by username", notes = "Retrive the userPreference by their username")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = UserPreferencesDto.class),
+        @ApiResponse(code = 400, message = "Bad Request")})
     public UserPreferencesDto getByUsername(@PathVariable String username) {
         LOGGER.debug("getByUsername method has been called with the username: {}", username);
         Meter requests = metrics.meter("getByUsername");
         requests.mark();
         return userPreferencesService.getByUsername(username);
     }
-
+    
+    @ApiOperation(value = "Retrieve all userPreference", notes = "Retrieve all the userPreferences in the system")
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = UserPreferencesDto.class)})
     public List<UserPreferencesDto> getAll() {
         LOGGER.debug("GetAll method has been called");
         Meter requests = metrics.meter("getAll");
@@ -62,6 +78,7 @@ public class UserPreferencesController {
     }
 
     @RequestMapping(value = "/{username}", method = RequestMethod.DELETE)
+    @ApiOperation(value = "Delete a userPreference", notes = "For given username, find userPreference and delete.")
     public void delete(@PathVariable String username) {
         LOGGER.debug("Delete method with username {} has been called", username);
         userPreferencesService.delete(username);
