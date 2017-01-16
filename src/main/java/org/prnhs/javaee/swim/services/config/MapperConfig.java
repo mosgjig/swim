7676ecbc9745c8c3cfd27e1a4992eaee48ba4201
MirlindQeriqi <mirlindqeriqi@gmail.com>
2017-01-16 19:31:26 +0100

    mq - dozer - test class fixed

--- src/main/java/org/prnhs/javaee/swim/services/config/MapperConfig.java
+++ src/main/java/org/prnhs/javaee/swim/services/config/MapperConfig.java
@@ -7,8 +7,10 @@ import org.dozer.classmap.RelationshipType;
 import org.dozer.loader.api.BeanMappingBuilder;
 import static org.dozer.loader.api.FieldsMappingOptions.collectionStrategy;
 import static org.dozer.loader.api.FieldsMappingOptions.oneWay;
+import org.prnhs.javaee.swim.core.entity.Program;
 import org.prnhs.javaee.swim.dto.MyObjectA;
 import org.prnhs.javaee.swim.dto.MyObjectB;
+import org.prnhs.javaee.swim.dto.ProgramDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,8 +39,10 @@ public class MapperConfig {
                         .fields(PASSWORD, PASSWORD, oneWay())
                         .fields(MY_LIST, MY_LIST, collectionStrategy(Boolean.TRUE, RelationshipType.NON_CUMULATIVE))
                         .fields(MY_USERS, MY_USERS, collectionStrategy(Boolean.FALSE, RelationshipType.NON_CUMULATIVE));
-                
+                mapping(Program.class, ProgramDto.class)
+                        .fields(field("id"), field("key"), oneWay())
+                        .fields(field("objective"), field("objective"));
             }
         };
-    }
+    }    
 }
